73e5bd92a45623ebe77ceab40ea961d3c62ff7f7
Enrico Mrass <enrico.mrass@googlemail.com>
2014-04-01 01:49:06 +0200

    Fix config unix_socket_directory

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,24 +1,12 @@
 case node['platform']
   when "ubuntu"
 
-  case
-  when node['platform_version'].to_f <= 9.04
-    default['postgresql']['version'] = "8.3"
-  when node['platform_version'].to_f <= 11.04
-    default['postgresql']['version'] = "8.4"
-  else
-    default['postgresql']['enable_pgdg_apt'] = true
-    default['postgresql']['version'] = "9.3"
-  end
-
+  default['postgresql']['enable_pgdg_apt'] = true
+  default['postgresql']['version'] = "9.3"
+  
   default['postgresql']['dir'] = "/etc/postgresql/#{node['postgresql']['version']}/main"
-  case
-  when (node['platform_version'].to_f <= 10.04) && (! node['postgresql']['enable_pgdg_apt'])
-    default['postgresql']['server']['service_name'] = "postgresql-#{node['postgresql']['version']}"
-  else
-    default['postgresql']['server']['service_name'] = "postgresql"
-  end
-
+  default['postgresql']['server']['service_name'] = "postgresql"
+  
   default['postgresql']['client']['packages'] = ["postgresql-client-#{node['postgresql']['version']}","libpq-dev"]
   default['postgresql']['server']['packages'] = ["postgresql-#{node['postgresql']['version']}"]
   default['postgresql']['contrib']['packages'] = ["postgresql-contrib-#{node['postgresql']['version']}"]
@@ -31,5 +19,6 @@ case node['platform']
   default['postgresql']['config']['hba_file'] = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_hba.conf"
   default['postgresql']['config']['ident_file'] = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_ident.conf"
   default['postgresql']['config']['external_pid_file'] = "/var/run/postgresql/#{node['postgresql']['version']}-main.pid"
-  default['postgresql']['config']['unix_socket_directories'] = '/var/run/postgresql' if node['postgresql']['version'].to_f >= 9.3
+  default['postgresql']['config']['unix_socket_directory'] = nil                     # remove this parameter, which is inserted by default using v9.1
+  default['postgresql']['config']['unix_socket_directories'] = '/var/run/postgresql' # valid from 9.3 on
 end
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 
 depends 'postgresql'
