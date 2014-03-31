262b4ec4cb0a20f225acdb48376de9711245f95b
Enrico Mrass <enrico.mrass@googlemail.com>
2014-04-01 01:03:14 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,5 @@
 name             'tippfuchs-postgresql'
 maintainer       'Enrico Mrass'
-maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
