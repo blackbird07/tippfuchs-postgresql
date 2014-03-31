ab8cff6a8acfa25a89f24816077573fd11b41014
Enrico Mrass <enrico.mrass@googlemail.com>
2014-04-01 01:38:29 +0200

    Version 1.0.1 Release

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description      'Installs/Configures tippfuchs-postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
+
 depends 'postgresql'
 
