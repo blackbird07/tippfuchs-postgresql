016d8970d8141a4561e602e25321a66045f0919b
Enrico Mrass <github@emrass.de>
2014-05-09 20:29:22 +0200

    Update to Postgres Cookbook 3.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.0.7'
 
 
-depends 'postgresql', '~> 3.3.0'
-
+depends 'postgresql', '~> 3.4.0'
